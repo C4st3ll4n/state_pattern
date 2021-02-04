8dad1bff3795708a45dea36b5a40dd1327534099
Pedro Henrique <p13dr0h@gmail.com>
2021-02-04 00:55:10 -0300

    added the no card usecase

--- src/main/java/NoCard.java
+++ src/main/java/NoCard.java
@@ -7,21 +7,22 @@ public class NoCard implements  ATMState{
 
     @Override
     public void insertCard() {
-
+        System.out.println("Please insert your PIN");
+        machine.setState(machine.getYesCardState());
     }
 
     @Override
     public void ejectCard() {
-
+        System.out.println("Enter a card first");
     }
 
     @Override
     public void insertPin(String pin) {
-
+        System.out.println("Enter a card first");
     }
 
     @Override
     public void requestCash(double amount) {
-
+        System.out.println("Enter a card first");
     }
 }
