ff87c6622397d8415d2fecbe4afe5b4b88dc51dc
Pedro Henrique <p13dr0h@gmail.com>
2021-02-04 01:39:56 -0300

    usecases implemented

--- src/main/java/HasPin.java
+++ src/main/java/HasPin.java
@@ -8,21 +8,35 @@ public class HasPin implements ATMState {
 
     @Override
     public void insertCard() {
-
+        System.out.println("You can't enter more than one card");
     }
 
     @Override
     public void ejectCard() {
-
+        System.out.println("Card ejected");
+        machine.setState(machine.getNoCardState());
     }
 
     @Override
     public void insertPin(String pin) {
-
+        System.out.println("PIN already entered");
     }
 
     @Override
     public void requestCash(double amount) {
+        if(amount > machine.cashInMachine){
+            System.out.println("Not enough cash !");
+            ejectCard();
+        }
+        else if(machine.cashInMachine <= 0){
+            System.out.println("Not enough cash !");
+            machine.setState(machine.getNoCashState());
 
+        }
+        else{
+            System.out.println(amount+" provided by ATM");
+            machine.setCashInMachine(machine.cashInMachine - amount);
+            ejectCard();
+        }
     }
 }
--- src/main/java/NoCash.java
+++ src/main/java/NoCash.java
@@ -8,21 +8,21 @@ public class NoCash implements  ATMState{
 
     @Override
     public void insertCard() {
-
+        System.out.println("Not cash !!");
     }
 
     @Override
     public void ejectCard() {
-
+        System.out.println("Not cash !!\nYou didn't enter a card");
     }
 
     @Override
     public void insertPin(String pin) {
-
+        System.out.println("Not cash !!");
     }
 
     @Override
     public void requestCash(double amount) {
-
+        System.out.println("Not cash !!");
     }
 }
